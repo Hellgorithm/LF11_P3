4a4ea83299de05f897821994744a374249068d26
Hellgorithm <kain.private@protonmail.com>
2025-01-30 14:35:29 +0100

    added read config function, restructured config to all be in one json file

--- DomainConfiguration.ps1
+++ DomainConfiguration.ps1
@@ -1,7 +1,8 @@
-﻿[string]$configFolderPath = $PSScriptroot + "\configs\" # Folder containing the Config Files for the Domain Configuration
-[string]$userFilePath = $configFolderPath + "users.json" # JSON File containing Users, Groups to assign to them, and other attributes for User creation.
-[string]$domainStructureFilePath = $configFolderPath + "structure.json"  # JSON File containing OUs and attributes
-[string]$logFilePath = $configFolderPath + "DomainConfig.log" # Log File for the Domain Configuration Script
+﻿[string]$configFolderPath = $PSScriptRoot + "\configs\" # Folder containing the Config Files for the Domain Configuration
+[string]$configFilePath = $configFolderPath + "domainConfig.json" # JSON File containing Users, Groups to assign to them, and other attributes for User creation.
+[string]$logFilePath = $PSScriptRoot + "DomainConfig.log" # Log File for the Domain Configuration Script
+[string]$serverUNC = "\\$env:COMPUTERNAME" # UNC Path to the Server
+$allUsers = @() # Array containing all Users to be created, is filled once the Config File is read
 
 
 class User {
@@ -11,11 +12,12 @@ class User {
     [string[]]$groups
     [string]$homeShare
 
-    User($name, $surname, $loginName, $groups) {
+    User($name, $surname, $loginName, $groups, $serverUNC) {
         $this.name = $name
         $this.surname = $surname
         $this.loginName = $loginName
         $this.groups = $groups
+        $this.homeShare = $serverUNC + $loginName + "$"
     }
 
     # [void] DisplayInfo() {
@@ -23,6 +25,13 @@ class User {
     # }
 }
 
+
+
+
+function Prune-Log(){
+    $logContent = Get-Content -Path $logFilePath
+    $logContent | Select-Object -Last 100 | Set-Content -Path $logFilePath
+}
 
 function Write-LogMessage($m, $e){
     $logMessage = "$(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n    - $m"
@@ -36,19 +45,31 @@ function Write-LogMessage($m, $e){
     }
 }
 
-function Prune-Log(){
-    $logContent = Get-Content -Path $logFilePath
-    $logContent | Select-Object -Last 100 | Set-Content -Path $logFilePath
-}
-
 Switch (Test-Path $configFolderPath){
     $false {throw "Config Path could not be found!"}
     $true {return}
 }
 
 # Reads and Imports the Config Files used for Domain creation
-function readConfigs(){
-    $DataBlob
+function readConfigs($selConfig){
+    $private:DataBlob = Get-Content $configFilePath | ConvertFrom-Json
+    switch ($selConfig) {
+        "user" { $userConfig = $DataBlob.Users }
+        "group" { $groupConfig = $DataBlob.Groups }
+        "ou" { $ouConfig = $DataBlob.OUs }
+        "share" { $shareConfig = $DataBlob.Folders }
+        "dhcp" { $dhcpConfig = $DataBlob.DHCP }
+        Default { 
+            $private:userConfig = $DataBlob.Users
+            $Script:groupConfig = $DataBlob.Groups
+            $Script:ouConfig = $DataBlob.OUs
+            $Script:shareConfig = $DataBlob.Folders
+            $Script:dhcpConfig = $DataBlob.DHCP
+        }
+    }
+    foreach ($user in $userConfig){
+        $allUsers += User::new($user.Name, $user.Surname, $user.LoginName, $user.Groups, $serverUNC)
+    }
 }
 
 
@@ -59,6 +80,6 @@ function createDomainStructure(){
 
 # Handles Importing Users and Registering them in ADS
 function registerUsers(){
-    Get-Content $userFilePath | ConvertFrom-Json -AsHashtable
+    
 
 }
