2d68bde80c3f0fa37dcccc662a2c4252e80a5d6b
Hellgorithm <168771949+Hellgorithm@users.noreply.github.com>
2025-01-30 11:27:01 +0100

    Created Domain Config Script

---
+++ DomainConfiguration.ps1
@@ -0,0 +1,36 @@
+﻿[string]$configFolderPath = "C:\DomainConfig\"
+[string]$userFilePath = $configFolderPath + "users.json" # JSON File containing Users, Groups to assign to them, and other attributes for User creation.
+[string]$domainStructureFilePath = $configFolderPath + "structure.json"  # JSON File containing OUs and attributes
+[string]$logFilePath = $configFolderPath + "Get-Date -Format YYMMDDhh"
+
+
+Class User{
+
+
+}
+
+
+function Write-ErrorLog($m, $e){
+   
+}
+
+Switch (Test-Path $configFolderPath){
+    $false {throw "Config Path could not be found!"}
+    $true {return}
+}
+
+# Reads and Imports the Config Files used for Domain creation
+function readConfigs(){
+    $Script:userTemplates = Get-Content $userFilePath | ConvertFrom-Json -AsHashtable
+}
+
+# Creates Custom OUs and similar within the Domain
+function createDomainStructure(){
+
+}
+
+# Handles Importing Users and Registering them in ADS
+function registerUsers(){
+    Get-Content $userFilePath | ConvertFrom-Json -AsHashtable
+
+}
