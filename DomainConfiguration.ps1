dea5890b47fec36d7ddcca6780873c1dc2f5f89c
Hellgorithm <kain.private@protonmail.com>
2025-01-30 12:28:37 +0100

    Added Config File, changed some stuff

--- DomainConfiguration.ps1
+++ DomainConfiguration.ps1
@@ -1,17 +1,44 @@
-﻿[string]$configFolderPath = "C:\DomainConfig\"
+﻿[string]$configFolderPath = $PSScriptroot + "\configs\" # Folder containing the Config Files for the Domain Configuration
 [string]$userFilePath = $configFolderPath + "users.json" # JSON File containing Users, Groups to assign to them, and other attributes for User creation.
 [string]$domainStructureFilePath = $configFolderPath + "structure.json"  # JSON File containing OUs and attributes
-[string]$logFilePath = $configFolderPath + "Get-Date -Format YYMMDDhh"
+[string]$logFilePath = $configFolderPath + "DomainConfig.log" # Log File for the Domain Configuration Script
 
 
-Class User{
+class User {
+    [string]$name
+    [string]$surname
+    [string]$loginName
+    [string[]]$groups
+    [string]$homeShare
 
+    User($name, $surname, $loginName, $groups) {
+        $this.name = $name
+        $this.surname = $surname
+        $this.loginName = $loginName
+        $this.groups = $groups
+    }
 
+    # [void] DisplayInfo() {
+    #     Write-Host "Name: $($this.Name), Surname: $($this.Surname)"
+    # }
 }
 
 
-function Write-ErrorLog($m, $e){
-   
+function Write-LogMessage($m, $e){
+    $logMessage = "$(Get-Date -Format "dd.MM.yyyy HH:mm:ss")`n    - $m"
+    if ($e -ne $null){
+        $logMessage += "`n    - Exception: $e"
+    }
+    Add-Content -Path $logFilePath -Value $logMessage
+    # Write-Host $logMessage
+    if ((Get-Item $logFilePath).Length -gt 2MB) {
+        Prune-Log
+    }
+}
+
+function Prune-Log(){
+    $logContent = Get-Content -Path $logFilePath
+    $logContent | Select-Object -Last 100 | Set-Content -Path $logFilePath
 }
 
 Switch (Test-Path $configFolderPath){
@@ -21,8 +48,9 @@ Switch (Test-Path $configFolderPath){
 
 # Reads and Imports the Config Files used for Domain creation
 function readConfigs(){
-    $Script:userTemplates = Get-Content $userFilePath | ConvertFrom-Json -AsHashtable
+    $DataBlob
 }
+
 
 # Creates Custom OUs and similar within the Domain
 function createDomainStructure(){
